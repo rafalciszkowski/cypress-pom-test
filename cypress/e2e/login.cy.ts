36dea41558c4f67438e8722072940c4e57a8e80a
rafalciszkowski <ciszkowski.rafal@gmail.com>
2024-10-27 17:05:26 +0100

    code cleaning

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,37 +1,37 @@
-import LoginPage from "../pages/LoginPage";
-import TodoPage from "../pages/TodoPage";
-import { invalidLogin } from "../fixtures/testData";
+import LoginPage from "../pages/LoginPage"
+import TodoPage from "../pages/TodoPage"
+import { invalidLogin } from "../fixtures/testData"
 
 describe('Login Functionality Tests', () => {
     
-    let loginPage: LoginPage;
-    let todoPage: TodoPage;
+    let loginPage: LoginPage
+    let todoPage: TodoPage
 
     /**
      * Initializes the pages and loads the login page before each test.
      */
     beforeEach(() => {
-        loginPage = new LoginPage();
-        todoPage = new TodoPage();
-        loginPage.load();
-    });
+        loginPage = new LoginPage()
+        todoPage = new TodoPage()
+        loginPage.load()
+    })
 
     /**
      * Tests the login functionality with valid credentials.
      */
     it('should successfully log in with valid credentials', () => {
-        loginPage.login();
-        todoPage.noTodosElement.should('be.visible');
-    });
+        loginPage.login()
+        todoPage.noTodosElement.should('be.visible')
+    })
 
     /**
      * Tests the login functionality with invalid credentials.
      * Iterates over the invalidLogin array and performs login attempts with each set of invalid credentials.
      */
     Cypress._.times(invalidLogin.length, (index) => {
-        it(`should display error for invalid login - ${invalidLogin[index].type}`, () => {
-            loginPage.loginWithInvalidData(invalidLogin[index], invalidLogin[index].type);
-            loginPage.validateErrorMessage(invalidLogin[index].type, index);
-        });
-    });
-});
+        it(`should display error for invalid login - type: ${invalidLogin[index].type}`, () => {
+            loginPage.loginWithInvalidData(invalidLogin[index], invalidLogin[index].type)
+            loginPage.validateErrorMessage(invalidLogin[index].type, index)
+        })
+    })
+})
--- cypress/pages/LoginPage.ts
+++ cypress/pages/LoginPage.ts
@@ -1,18 +1,18 @@
-import CustomResponse from "../interfaces/CustomResponse";
-import RegisterPage from "./RegisterPage";
-import { invalidLogin } from "../fixtures/testData";
-import InvalidLogin from "../interfaces/InvalidLogin";
+import CustomResponse from "../interfaces/CustomResponse"
+import RegisterPage from "./RegisterPage"
+import { invalidLogin } from "../fixtures/testData"
+import InvalidLogin from "../interfaces/InvalidLogin"
 
 export default class LoginPage {
 
-    registerPage: RegisterPage = new RegisterPage();
+    registerPage: RegisterPage = new RegisterPage()
 
     /**
      * Gets the email input element.
      * @returns {Cypress.Chainable<JQuery<HTMLElement>>} The email input element.
      */
     get emailInput(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get('[data-testid="email"]');
+        return cy.get('[data-testid="email"]')
     }
 
     /**
@@ -20,7 +20,7 @@ export default class LoginPage {
      * @returns {Cypress.Chainable<JQuery<HTMLElement>>} The password input element.
      */
     get passwordInput(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get('[data-testid="password"]');
+        return cy.get('[data-testid="password"]')
     }
 
     /**
@@ -28,7 +28,7 @@ export default class LoginPage {
      * @returns {Cypress.Chainable<JQuery<HTMLElement>>} The login button element.
      */
     get loginButton(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get('[data-testid="submit"]');
+        return cy.get('[data-testid="submit"]')
     }
 
     /**
@@ -36,7 +36,7 @@ export default class LoginPage {
      * @returns {Cypress.Chainable<JQuery<HTMLElement>>} The email error message element.
      */
     get emailError(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get('#email-helper-text');
+        return cy.get('#email-helper-text')
     }
 
     /**
@@ -44,7 +44,7 @@ export default class LoginPage {
      * @returns {Cypress.Chainable<JQuery<HTMLElement>>} The password error message element.
      */
     get passwordError(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get('#password-helper-text');
+        return cy.get('#password-helper-text')
     }
 
     /**
@@ -52,7 +52,7 @@ export default class LoginPage {
      * @returns {Cypress.Chainable<JQuery<HTMLElement>>} The alert message element.
      */
     get alertMessage(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get('[data-testid="error-alert"] .MuiAlert-message');
+        return cy.get('[data-testid="error-alert"] .MuiAlert-message')
     }
 
     /**
@@ -60,7 +60,7 @@ export default class LoginPage {
      * @returns {Cypress.Chainable<Window>} The window object of the loaded page.
      */
     load(): Cypress.Chainable<Window> {
-        return cy.visit('/login');
+        return cy.visit('/login')
     }
 
     /**
@@ -68,12 +68,12 @@ export default class LoginPage {
      */
     login(): void {
         this.registerPage.registerUsingApi().then((response: CustomResponse) => {
-            const { email, password } = JSON.parse(response.requestBody);
+            const { email, password } = JSON.parse(response.requestBody)
 
-            this.emailInput.type(email);
-            this.passwordInput.type(password);
-            this.loginButton.click();
-        });
+            this.emailInput.type(email)
+            this.passwordInput.type(password)
+            this.loginButton.click()
+        })
     }
 
     /**
@@ -82,19 +82,19 @@ export default class LoginPage {
      * @param {string} type - The type of invalid login attempt.
      */
     loginWithInvalidData(credentials: InvalidLogin, type: string): void {
-        this.emailInput.type(credentials.email);
+        this.emailInput.type(credentials.email)
 
         if (credentials.password === 'empty') {
-            this.passwordInput.type(credentials.password).clear();
+            this.passwordInput.type(credentials.password).clear()
         } else {
-            this.passwordInput.type(credentials.password);
+            this.passwordInput.type(credentials.password)
         }
 
         if (!['incorrect email', 'empty password'].includes(type)) {
-            cy.intercept('/api/v1/users/login').as('loginResponse');
+            cy.intercept('/api/v1/users/login').as('loginResponse')
         }
 
-        this.loginButton.click();
+        this.loginButton.click()
     }
 
     /**
@@ -103,37 +103,37 @@ export default class LoginPage {
      * @param {number} index - The index of the invalid login data in the array.
      */
     validateErrorMessage(type: string, index: number): void {
-        const errorMessage = invalidLogin[index].errorMessage;
+        const errorMessage = invalidLogin[index].errorMessage
 
         const checkResponse = (code: number, message: string): void => {
             cy.wait('@loginResponse').should(({ response }) => {
-                expect(response.statusCode).to.eq(code);
-                expect(response.statusMessage).to.eq(message);
-            });
-        };
+                expect(response.statusCode).to.eq(code)
+                expect(response.statusMessage).to.eq(message)
+            })
+        }
 
         switch (type) {
             case 'incorrect email':
-                this.emailError.should('have.text', errorMessage);
-                break;
+                this.emailError.should('have.text', errorMessage)
+                break
 
             case 'empty password':
-                this.passwordError.should('have.text', errorMessage);
-                break;
+                this.passwordError.should('have.text', errorMessage)
+                break
 
             case 'password not valid':
             case 'no email in DB':
-                checkResponse(400, 'Bad Request');
-                this.alertMessage.should('have.text', errorMessage);
-                break;
+                checkResponse(400, 'Bad Request')
+                this.alertMessage.should('have.text', errorMessage)
+                break
 
             case 'email/password combination not correct':
-                checkResponse(401, 'Unauthorized');
-                this.alertMessage.should('have.text', errorMessage);
-                break;
+                checkResponse(401, 'Unauthorized')
+                this.alertMessage.should('have.text', errorMessage)
+                break
 
             default:
-                throw new Error(`Unknown error type: ${type}`);
+                throw new Error(`Unknown error type: ${type}`)
         }
     }
 }
